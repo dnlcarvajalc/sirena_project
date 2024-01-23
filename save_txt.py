ab5704f4b27598e32d3adb2ae3c0466c6dbee73e
Daniela0308 <danielavillamizartapias@gmail.com>
2024-01-23 14:51:29 -0500

    HU3 con cambios sugeridos

--- save_txt.py
+++ save_txt.py
@@ -1,30 +1,33 @@
 import os
 import json
-from os import makedirs
-import datetime as dt
+from datetime import datetime
 
 
 def save_dates():
+    """Esta función crea una estructura de carpetas nombradas con la información de
+    la fecha actual en el orden de año, mes y día (YYYY/MM/DD).
+    Además genera un archivo de texto plano (.json) que guarda la información de la hora actual.
+    """
     #Fecha y hora actual
-    current_date_and_time = dt.datetime.now()
-    yyyy = current_date_and_time.strftime("%Y")
-    mm = current_date_and_time.strftime("%m")
-    dd = current_date_and_time.strftime("%w")
+    current_date_and_time = datetime.now()
+    YYYY = current_date_and_time.strftime("%Y")
+    MM = current_date_and_time.strftime("%m")
+    DD = current_date_and_time.strftime("%j")
     hh = current_date_and_time.strftime("%H")
     mm = current_date_and_time.strftime("%M")
     ss = current_date_and_time.strftime("%S")
 
     #Crear archivo hh_mm_ss.json
-    file_name_js = 'hh_mm_ss.json'
+    file_name_js = hh + '_' + mm + '_' + ss + '.json'
     data_js = {'hh': hh , 'mm': mm , 'ss': ss}
 
     #Obtener ruta de la carpeta actual
     folder_path = os.path.abspath(os.getcwd())
     #Crear la ruta '/YYYY/MM/DD' en la carpeta actual
-    os.makedirs(folder_path + '/' + str(yyyy) + '/' + str(mm) + '/' + str(dd), exist_ok=True)
+    os.makedirs(folder_path + '/' + str(YYYY) + '/' + str(MM) + '/' + str(int(DD)), exist_ok=True)
 
     #Guardar archivo hh_mm_ss.json en la ruta creada
-    with open(os.path.join(folder_path +  '/' + str(yyyy) + '/' + str(mm) + '/' + str(dd), file_name_js), 'w') as file:
+    with open(os.path.join(folder_path +  '/' + str(YYYY) + '/' + str(MM) + '/' + str(int(DD)), file_name_js), 'w') as file:
         json.dump(data_js, file)
 
 save_dates()
